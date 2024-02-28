50314a63e86c41fc1d83f077dde80b1507066114
Aakash Rao <aakash.codes02@gmail.com>
2024-02-28 22:49:06 +0530

    Changes to the file Structure

--- pages/contact.js
+++ pages/contact.js
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Form from '@/components/Form';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
+import Maps from '@/components/Maps';
 
 export default function Contact() {
   return (
@@ -15,18 +16,7 @@ export default function Contact() {
       <Header />
       <section className="bg-white min-h-screen flex justify-center flex-col lg:flex-row items-center relative p-4">
         <Form />
-        <div className="aspect-video rounded">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3718.01520230535!2d81.66422358194303!3d21.27086581283316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a28e78a3972d979%3A0x61cbc983d1fc74c6!2sAnant%20Vihar%20Colony%2C%20Dubey%20Colony%2C%20Mowa%2C%20Raipur%2C%20Chhattisgarh%20492014!5e0!3m2!1sen!2sin!4v1709136235288!5m2!1sen!2sin"
-            width="500"
-            height="600"
-            style={{ border: 0 }}
-            className="overflow-hidden"
-            allowFullScreen=""
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          ></iframe>
-        </div>
+        <Maps />
       </section>
       <Footer />
     </>
